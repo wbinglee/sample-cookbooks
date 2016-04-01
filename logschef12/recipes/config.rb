9e3b7314d64ef45cb6e1ba4655bd897b341457ef
Bruce Li <libruce@amazon.com>
2016-04-01 16:28:08 +0200

    Update cookbook

--- logschef12/recipes/config.rb
+++ logschef12/recipes/config.rb
@@ -4,4 +4,8 @@ template "/tmp/cwlogs.cfg" do
   owner "root"
   group "root"
   mode 0644
+  variables(
+    :instance => search("aws_opsworks_instance").first,
+    :stack => search("aws_opsworks_stack").first['name']
+  )
 end
